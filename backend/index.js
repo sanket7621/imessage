674014c6f856a1bb56a7b57e87f21918b08f3b26
Sanket7621 <sj903065@gmail.com>
2026-07-17 02:57:26 +0530

    fix: update route handling to use regex for matching all paths

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(clerkMiddleware());
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
 
-  app.get("*", (req, res, next) => {
+  app.get(/.*/,  (req, res, next) => {
     res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
   });
 }

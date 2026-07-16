393fb013ca0521c2209f29bf4c13707118133eed
Sanket7621 <sj903065@gmail.com>
2026-07-17 01:32:49 +0530

    Add Dockerfile and .dockerignore; update backend package.json with new scripts and dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import  cors from "cors";
 import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
+
 import { clerkMiddleware } from "@clerk/express";
 
 import user from "./src/models/user.model.js";
@@ -13,6 +17,14 @@ const frontendurl = process.env.FRONTEND_URL
 app.use(express.json());
 app.use(cors({origin:frontendurl, credentials: true}));
 app.use(clerkMiddleware());
+
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
